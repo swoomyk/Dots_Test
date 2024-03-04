3a22d7ccdd23c1534a561327d37d8b02b6ae57d5
swoomyk <160307331+swoomyk@users.noreply.github.com>
2024-03-04 21:40:17 +0500

    Update Testingclass.java

--- Testingclass.java
+++ Testingclass.java
@@ -1,5 +1,5 @@
 public class Testingclass {
     public static void main(String[] args) {
-        System.out.println("Testing class push completed");
+        System.out.println("Testing class push completed NEW");    
     }
 }
