66ab8f44f5848e89b3457ccfa4ce7ba61669ef5e
swoomyk <160307331+swoomyk@users.noreply.github.com>
2024-03-07 17:35:14 +0500

    Update Testingclass.java

--- Testingclass.java
+++ Testingclass.java
@@ -2,6 +2,7 @@ public class Testingclass {
     public static void main(String[] args) {
         System.out.println("PUSH N BACK");
         System.out.println("pushnback#2");
+        System.out.println("pullnback");
 
     }
 }
